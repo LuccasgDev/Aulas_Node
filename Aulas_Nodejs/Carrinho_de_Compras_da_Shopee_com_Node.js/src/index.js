ed0f2d80e7d003428f7c371e70aa2f3245a30118
LuccasgDev <163527503+LuccasgDev@users.noreply.github.com>
2025-07-25 09:30:05 -0300

    Show

--- Aulas_Nodejs/Carrinho_de_Compras_da_Shopee_com_Node.js/src/index.js
+++ Aulas_Nodejs/Carrinho_de_Compras_da_Shopee_com_Node.js/src/index.js
@@ -1,1 +1,25 @@
-console.log("!")
+import * as cartService from "./services/cart.js"
+import createItem from "./services/item.js"
+
+const myCart = []
+const myWishList = []
+
+console.log("Bem vindo ao seu carrinho da Shopee!")
+
+const item1 = await createItem("Keyboard", 169.29, 1)
+const item2 = await createItem("Mouse", 69.29, 3)
+const item3 = await createItem("Mouse", 169.29, 1)
+
+await cartService.addItem(myCart, item1)
+await cartService.addItem(myCart, item2)
+
+await cartService.removeItem(myCart, item2)
+await cartService.removeItem(myCart, item2)
+
+// await cartService.addItem(myWishList, item3)
+
+// await cartService.deleteItem(myCart, item2.name)
+
+await cartService.displayCart(myCart)
+
+await cartService.calculateTotal(myCart)
